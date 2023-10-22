ee20641a32d822622d5482df9b1029bd9912ef51
Zharaskhan <55748394+ZharaskhanAman@users.noreply.github.com>
2023-10-22 23:53:47 +0600

    Resolve linter errors

--- judge/views/api/api.py
+++ judge/views/api/api.py
@@ -1,13 +1,15 @@
+import json
+
 from django.conf import settings
 from django.contrib.auth.models import User
-from judge.models import Language, Profile, Organization
-from django.shortcuts import get_object_or_404
 from django.db import IntegrityError, transaction
-from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 from django.utils.decorators import method_decorator
 from django.views import View
-import json
+from django.views.decorators.csrf import csrf_exempt
+
+from judge.models import Language, Organization, Profile
 
 
 def register_user(username, password, full_name, org_id):
@@ -23,10 +25,9 @@ def register_user(username, password, full_name, org_id):
 
 
 def _create_user(username, password):
-    """Creates and returns a new user."""
     user = User(
         username=username,
-        email=f"{username}@esep.cpfed.kz",
+        email=f'{username}@esep.cpfed.kz',
         is_active=True,
         is_superuser=False,
         is_staff=False,
@@ -36,13 +37,12 @@ def _create_user(username, password):
     try:
         user.save()
     except IntegrityError:
-        raise ValueError(f"User with username {username} already exists.")
+        raise ValueError(f'User with username {username} already exists.')
 
     return user
 
 
 def _create_profile_for_user(user, full_name, org):
-    """Creates a profile for the given user and associates it with the specified organization."""
     profile = Profile(
         user=user,
         language=Language.objects.get(key=settings.DEFAULT_USER_LANGUAGE),
@@ -55,30 +55,30 @@ def _create_profile_for_user(user, full_name, org):
         profile.save()
         profile.organizations.add(org)
     except IntegrityError:
-        raise ValueError(f"Error creating profile for user {user.username}.")
+        raise ValueError(f'Error creating profile for user {user.username}.')
 
 
-@method_decorator(csrf_exempt, name="dispatch")
+@method_decorator(csrf_exempt, name='dispatch')
 class SyncUsersFromCPFEDView(View):
     def post(self, request, token, *args, **kwargs):
         try:
             if token != settings.CPFED_TOKEN:
-                return JsonResponse({"detail": "Unauthorized"}, status=403)
+                return JsonResponse({'detail': 'Unauthorized'}, status=403)
 
             data = json.loads(request.body)
 
-            username = data.get("username")
-            password = data.get("password")
-            full_name = data.get("full_name")
-            org_id = data.get("org_id")
+            username = data.get('username')
+            password = data.get('password')
+            full_name = data.get('full_name')
+            org_id = data.get('org_id')
 
             if not all([username, password, full_name, org_id]):
                 return JsonResponse(
-                    {"detail": "Missing required parameters."}, status=400
+                    {'detail': 'Missing required parameters.'}, status=400,
                 )
 
             register_user(username, password, full_name, org_id)
 
-            return JsonResponse({"detail": "User registered successfully"}, status=201)
+            return JsonResponse({'detail': 'User registered successfully'}, status=201)
         except Exception as e:
-            return JsonResponse({"detail": str(e)}, status=400)
+            return JsonResponse({'detail': str(e)}, status=400)
